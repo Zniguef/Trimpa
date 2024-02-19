d9c049f4de59cad624d09f4011279d7faf32d433
zniguefmohamed <mohamedzniguef@gmail.com>
2024-02-19 21:07:48 +0100

    Setup Database conection

---
+++ backend/src/main/java/Trimpa/Trimpa/TrimpaApplication.java
@@ -0,0 +1,13 @@
+package Trimpa.Trimpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrimpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrimpaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/Trimpa/Trimpa/TrimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package Trimpa.Trimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
